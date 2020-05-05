fdd1114b1efb9cb5c6cf6294c66e0a2ee7227b41
lihe <13718259084@163.com>
2020-05-05 09:29:51 +0800

    兼容数据问题

--- app/service/MenuService.php
+++ app/service/MenuService.php
@@ -5,20 +5,33 @@ use app\model\Menu;
 class MenuService
 {
 
-    public function getFather($father){
+    public function getFather($father = []){
+        if($father){
+            return Menu::all([
+                'status'	=>	0,
+                'level'		=>	0,
+                'id' => ['in', $father]
+            ]);
+        }
 
         return Menu::all([
-        	'status'	=>	0,
-        	'level'		=>	0,
-            'id' => ['in', $father]
+            'status'	=>	0,
+            'level'		=>	0
         ]);
     }
   
-    public function getChild($child){
+    public function getChild($child = []){
+        if($child){
+            return Menu::all([
+                'status'	=>	0,
+                'level'		=>	['>',0],
+                'id' => ['in', $child]
+            ]);
+        }
+
         return Menu::all([
-        	'status'	=>	0,
-        	'level'		=>	['>',0],
-            'id' => ['in', $child]
+            'status'	=>	0,
+            'level'		=>	['>',0]
         ]);
     }
 
