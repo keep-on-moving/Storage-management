39f7a689ee0c750c4e7def89f10a3d61dd28bb24
lihe <13718259084@163.com>
2020-05-08 17:27:48 +0800

    增加导出功能 不能页面优化

--- app/controller/Instorage.php
+++ app/controller/Instorage.php
@@ -1,5 +1,6 @@
 <?php
 namespace app\controller;
+use app\service\ExcelExportService;
 use app\service\InstorageService,
     app\model\Product,
     app\model\Supplier,
@@ -85,6 +86,42 @@ class Instorage extends Base
     public function barcode($sn){
         include ROOT_PATH.'/extend/Barcode.php';
         die( \Barcode::code39($sn) );  
+    }
+
+    public function export(){
+        $param = Request::instance()->get();
+        $data = $this->service->page()->toArray();
+        $name=$param['start_time'].'至'.$param['end_time'].'入库单';
+        $header=[['id','出库单id'],['sn','出库单编号'],['author', '制表人'],['supplier', '供应商'], ['type', '出库类型'], ['instorage_checker_a','入库审核人A'], ['instorage_checker_b','入库审核人B'], ['add_time','创单时间'],['info','入库单详情'] ,['desc','备注']];
+        $newdata = [];
+        foreach ($data['data'] as $key => $value){
+            $newdata[$key]['id'] = $value['id'];
+            $newdata[$key]['sn'] = $value['sn'];
+            $newdata[$key]['author'] = $value['author'];
+            $newdata[$key]['supplier'] = $value['supplier'];
+            $newdata[$key]['type'] = $value['type'];
+            $newdata[$key]['instorage_checker_a'] = $value['instorage_checker_a'];
+            $newdata[$key]['instorage_checker_b'] = $value['instorage_checker_b'];
+            $newdata[$key]['add_time'] = date('Y年m月d日 H时i分s秒', $value['add_time']);
+            $detail = '';
+            $product = db('product')->find($value['product_id']);
+            if($value['type'] == '采购入库'){
+                $res = json_decode($value['res'], true);
+                foreach ($res as $val){
+                    $detail = $detail.'采购入库'.$product['name'].'的材料-材料ID：'.$val[0].'  材料名称：'.$val[1]. ' 入库数量：'.$val[2].$val[4]. ' 保质期：'.$val[3];
+                }
+                $detail = $detail.' 初步统计：'.$value['expected_num'];
+            }else{
+                $detail = '退货名称：'.$product['name'].'：退货数量：'.$value['return_num'].$product['unit']. '  退货保质期：'.$value['effective_time'].' 初步统计： '.$value['expected_num'];
+            }
+
+            $newdata[$key]['info'] = $detail;
+            $newdata[$key]['desc'] = $value['desc'];
+        }
+
+        $excel = new ExcelExportService();
+        $excel->exportExcel($name,$header,$newdata);
+
     }
 
 
--- app/controller/Outstorage.php
+++ app/controller/Outstorage.php
@@ -1,9 +1,11 @@
 <?php
 namespace app\controller;
+use app\service\ExcelExportService;
 use app\service\OutstorageService,
     app\model\Product,
     app\model\Supplier,
     app\model\Order;
+use think\Request;
 
 class Outstorage extends Base
 {
@@ -77,6 +79,32 @@ class Outstorage extends Base
         include ROOT_PATH.'/extend/Barcode.php';
         die( \Barcode::code39($sn) );  
     }
+    public function export(){
+        $param = Request::instance()->get();
+        $data = $this->service->page()->toArray();
+        $name=$param['start_time'].'至'.$param['end_time'].'出库单';
+        $header=[['id','出库单id'],['sn','出库单编号'],['author', '制表人'],['supplier', '供应商'], ['type', '出库类型'], ['outstorage_curator','保管员'], ['outstorage_consignee','提货人'], ['add_time','创单时间'],['info','出库单详情'] ,['desc','备注']];
+        $newdata = [];
+        foreach ($data['data'] as $key => $value){
+            $newdata[$key]['id'] = $value['id'];
+            $newdata[$key]['sn'] = $value['sn'];
+            $newdata[$key]['author'] = $value['author'];
+            $newdata[$key]['supplier'] = $value['supplier'];
+            $newdata[$key]['type'] = $value['type'];
+            $newdata[$key]['outstorage_curator'] = $value['outstorage_curator'];
+            $newdata[$key]['outstorage_consignee'] = $value['outstorage_consignee'];
+            $newdata[$key]['add_time'] = date('Y年m月d日 H时i分s秒', $value['add_time']);
+            $res = json_decode($value['res'], true);
+            $detail = '';
+            foreach ($res as $val){
+                $detail = $detail.'商品编号：'.$val[0].'  商品名称：'.$val[1]. ' 入库数量：'.$val[2].$val[5]. ' 包装：'.$val[4].' ||';
+            }
+            $newdata[$key]['info'] = $detail;
+            $newdata[$key]['desc'] = $value['desc'];
+        }
 
+        $excel = new ExcelExportService();
+        $excel->exportExcel($name,$header,$newdata);
 
+    }
 }
